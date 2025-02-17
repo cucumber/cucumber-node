4ba522094334187ed34360f74535d1b884f8d298
David Goss <david@davidgoss.co>
2025-02-17 00:23:30 +0000

    make test tolerate windows

--- test/integration/reporters.spec.ts
+++ test/integration/reporters.spec.ts
@@ -1,6 +1,7 @@
 import { stripVTControlCharacters } from 'node:util'
 import { expect } from 'chai'
 import { makeTestHarness } from '../utils.js'
+import path from 'node:path'
 
 describe('Reporters', () => {
   it('does not emit messages as diagnostics if no cucumber reporters', async () => {
@@ -38,8 +39,8 @@ Given('a step', () => {})`)
     const [output] = await harness.run('spec')
     const sanitised = stripVTControlCharacters(output.trim())
     expect(sanitised).to.include(`Multiple matching step definitions found for text "a step":
-  1) features/steps.js:2:1
-  2) features/steps.js:3:1`)
+  1) ${path.join('features', 'steps.js')}:2:1
+  2) ${path.join('features', 'steps.js')}:3:1`)
   })
 
   it('provides a useful error for an undefined step', async () => {
