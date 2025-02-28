739ff9b522aaab5110e1d8f47088ecc936793288
David Goss <david@davidgoss.co>
2025-02-28 13:39:59 +0000

    Add JUnit XML formatter

--- test/integration/reporters.spec.ts
+++ test/integration/reporters.spec.ts
@@ -4,58 +4,85 @@ import { makeTestHarness } from '../utils.js'
 import path from 'node:path'
 
 describe('Reporters', () => {
-  it('does not emit messages as diagnostics if no cucumber reporters', async () => {
-    const harness = await makeTestHarness()
-    await harness.writeFile(
-      'features/first.feature',
-      `Feature:
+  describe('spec', () => {
+    it('does not emit messages as diagnostics if no cucumber reporters', async () => {
+      const harness = await makeTestHarness()
+      await harness.writeFile(
+        'features/first.feature',
+        `Feature:
   Scenario:
     Given a step
     `
-    )
-    await harness.writeFile(
-      'features/steps.js',
-      `import { Given } from '@cucumber/node'
+      )
+      await harness.writeFile(
+        'features/steps.js',
+        `import { Given } from '@cucumber/node'
   Given('a step', () => {})
     `
-    )
-    const [output] = await harness.run('spec')
-    const sanitised = stripVTControlCharacters(output.trim())
-    expect(sanitised).not.to.include('@cucumber/messages:')
-  })
+      )
+      const [output] = await harness.run('spec')
+      const sanitised = stripVTControlCharacters(output.trim())
+      expect(sanitised).not.to.include('@cucumber/messages:')
+    })
 
-  it('provides a useful error for an ambiguous step', async () => {
-    const harness = await makeTestHarness()
-    await harness.writeFile(
-      'features/first.feature',
-      `Feature:
+    it('provides a useful error for an ambiguous step', async () => {
+      const harness = await makeTestHarness()
+      await harness.writeFile(
+        'features/first.feature',
+        `Feature:
   Scenario:
     Given a step`
-    )
-    await harness.writeFile(
-      'features/steps.js',
-      `import { Given } from '@cucumber/node'
+      )
+      await harness.writeFile(
+        'features/steps.js',
+        `import { Given } from '@cucumber/node'
 Given('a step', () => {})
 Given('a step', () => {})`
-    )
-    const [output] = await harness.run('spec')
-    const sanitised = stripVTControlCharacters(output.trim())
-    expect(sanitised).to.include(`Multiple matching step definitions found for text "a step":
+      )
+      const [output] = await harness.run('spec')
+      const sanitised = stripVTControlCharacters(output.trim())
+      expect(sanitised).to.include(`Multiple matching step definitions found for text "a step":
   1) ${path.join('features', 'steps.js')}:2:1
   2) ${path.join('features', 'steps.js')}:3:1`)
-  })
+    })
 
-  it('provides a useful error for an undefined step', async () => {
-    const harness = await makeTestHarness()
-    await harness.writeFile(
-      'features/first.feature',
-      `Feature:
+    it('provides a useful error for an undefined step', async () => {
+      const harness = await makeTestHarness()
+      await harness.writeFile(
+        'features/first.feature',
+        `Feature:
   Scenario:
     Given a step
     `
-    )
-    const [output] = await harness.run('spec')
-    const sanitised = stripVTControlCharacters(output.trim())
-    expect(sanitised).to.include('No matching step definitions found for text "a step"')
+      )
+      const [output] = await harness.run('spec')
+      const sanitised = stripVTControlCharacters(output.trim())
+      expect(sanitised).to.include('No matching step definitions found for text "a step"')
+    })
+  })
+
+  describe('junit', () => {
+    it('outputs a junit xml report', async () => {
+      const harness = await makeTestHarness()
+      await harness.writeFile(
+        'features/first.feature',
+        `Feature:
+  Scenario:
+    Given a step
+    `
+      )
+      await harness.writeFile(
+        'features/steps.js',
+        `import { Given } from '@cucumber/node'
+  Given('a step', () => {})
+    `
+      )
+
+      const [output] = await harness.run('@cucumber/node/reporters/junit')
+
+      expect(output).to.include(
+        '<system-out><![CDATA[Given a step................................................................passed]]></system-out>'
+      )
+    })
   })
 })
