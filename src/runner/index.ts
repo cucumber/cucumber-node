fe763ac25a7641d16959259fbee5d8d78b5e526d
David Goss <david@davidgoss.co>
2025-11-16 13:06:44 +0000

    remove dead code

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,5 +1,3 @@
-import { suite, test } from 'node:test'
-
 import { makeTestPlan } from '@cucumber/core'
 
 import { newId } from '../newId.js'
@@ -9,26 +7,6 @@ import { messages } from './state.js'
 import { CompiledGherkin } from './types.js'
 
 export * from './types.js'
-
-export async function run(gherkin: CompiledGherkin) {
-  const plan = await prepare(gherkin)
-  await suite(plan.name, async () => {
-    for (const pickle of gherkin.pickles) {
-      const testCase = plan.select(pickle.id)
-      await test(testCase.name, async (ctx1) => {
-        await testCase.setup(ctx1)
-        for (const testStep of testCase.testSteps) {
-          await testStep.setup()
-          await ctx1.test(testStep.name, testStep.options, async (ctx2) => {
-            await testStep.execute(ctx2)
-          })
-          await testStep.teardown()
-        }
-        await testCase.teardown()
-      })
-    }
-  })
-}
 
 export async function prepare({ source, gherkinDocument, pickles }: CompiledGherkin) {
   messages.push({ source })
