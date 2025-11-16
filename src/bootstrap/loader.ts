7d33628f28149b5c6fab9f55de546d385a8e216b
David Goss <david@davidgoss.co>
2025-11-16 13:06:17 +0000

    avoid top level await

--- src/bootstrap/loader.ts
+++ src/bootstrap/loader.ts
@@ -52,24 +52,28 @@ function generateCode(gherkin: CompiledGherkin) {
   return `import { suite, test } from 'node:test'
 import { prepare } from '@cucumber/node/runner'
 
-const plan = await prepare(${JSON.stringify(gherkin)})
-await suite(plan.name, async () => {
-  ${gherkin.pickles
-    .map((pickle, index) => {
-      return `const testCase${index} = plan.select(${JSON.stringify(pickle.id)})
-      await test(testCase${index}.name, async (ctx1) => {
-        await testCase${index}.setup(ctx1)
-        for (const testStep of testCase${index}.testSteps) {
-          await testStep.setup()
-          await ctx1.test(testStep.name, testStep.options, async (ctx2) => {
-            await testStep.execute(ctx2)
-          })
-          await testStep.teardown()
-        }
-        await testCase${index}.teardown()
-      })`
-    })
-    .join('\n')}
-})
+async function run() {
+  const plan = await prepare(${JSON.stringify(gherkin)})
+  await suite(plan.name, async () => {
+    ${gherkin.pickles
+      .map((pickle, index) => {
+        return `const testCase${index} = plan.select(${JSON.stringify(pickle.id)})
+        await test(testCase${index}.name, async (ctx1) => {
+          await testCase${index}.setup(ctx1)
+          for (const testStep of testCase${index}.testSteps) {
+            await testStep.setup()
+            await ctx1.test(testStep.name, testStep.options, async (ctx2) => {
+              await testStep.execute(ctx2)
+            })
+            await testStep.teardown()
+          }
+          await testCase${index}.teardown()
+        })`
+      })
+      .join('\n')}
+  })
+}
+
+run()
 `
 }
