dc2530c4146d17c859710789007ae150a7aa9d45
David Goss <david@davidgoss.co>
2025-11-17 08:17:13 +0000

    refactor test case lookup

--- src/runner/ExecutableTestPlan.ts
+++ src/runner/ExecutableTestPlan.ts
@@ -1,23 +1,30 @@
 import { AssembledTestCase, AssembledTestPlan } from '@cucumber/core'
+import { ensure } from '@cucumber/junit-xml-formatter/dist/src/helpers.js'
 
 import { ExecutableTestCase } from './ExecutableTestCase.js'
 import { WorldFactory } from './WorldFactory.js'
 
 export class ExecutableTestPlan {
+  private readonly testCaseByPickleId: Map<string, AssembledTestCase> = new Map()
+
   constructor(
     private readonly worldFactory: WorldFactory,
     private readonly plan: AssembledTestPlan
-  ) {}
+  ) {
+    for (const testCase of plan.testCases) {
+      this.testCaseByPickleId.set(testCase.pickleId, testCase)
+    }
+  }
 
   get name() {
     return this.plan.name
   }
 
   select(pickleId: string): ExecutableTestCase {
-    // TODO add pickleId to AssembledTestCase, make a Map<string, AssembledTestCase> on plan creation
-    const testCase = this.plan.testCases.find(
-      (tc) => tc.toMessage().pickleId === pickleId
-    ) as AssembledTestCase
+    const testCase = ensure(
+      this.testCaseByPickleId.get(pickleId),
+      'AssembledTestCase must exist for pickleId'
+    )
     return new ExecutableTestCase(this.worldFactory, testCase)
   }
 }
