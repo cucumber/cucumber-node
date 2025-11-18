13b69dc3680417f4f6b85cd482b53f7a18a71146
David Goss <david@davidgoss.co>
2025-11-18 13:21:36 +0000

    refactor ast generation

--- src/bootstrap/generateCode.ts
+++ src/bootstrap/generateCode.ts
@@ -2,8 +2,28 @@ import path from 'node:path'
 
 import { generate } from '@babel/generator'
 import * as t from '@babel/types'
+import { Pickle } from '@cucumber/messages'
 
 import { CompiledGherkin } from '../runner/index.js'
+
+export function generateCode(gherkin: CompiledGherkin): string {
+  const program = t.program([...createImports(), createSuite(gherkin)])
+
+  const output = generate(
+    program,
+    {
+      retainLines: false,
+      compact: false,
+      sourceMaps: true,
+      sourceFileName: gherkin.source.uri,
+    },
+    gherkin.source.data
+  )
+
+  const sourceMapComment = `//# sourceMappingURL=data:application/json;base64,${Buffer.from(JSON.stringify(output.map)).toString('base64')}`
+
+  return `${output.code}\n${sourceMapComment}`
+}
 
 function createImports() {
   return [
@@ -21,116 +41,33 @@ function createImports() {
   ]
 }
 
-function createSourceLocation(uri: string): t.SourceLocation {
-  return {
-    start: { line: 1, column: 0, index: 0 },
-    end: { line: 1, column: 0, index: 0 },
-    filename: path.basename(uri),
-    identifierName: undefined,
-  }
-}
-
-function withLoc<T extends t.Node>(node: T, loc: t.SourceLocation): T {
-  node.loc = loc
-  return node
-}
-
-function createTestStepLoop(testCaseVar: string, sourceLocation: t.SourceLocation) {
-  return t.forOfStatement(
-    t.variableDeclaration('const', [t.variableDeclarator(t.identifier('testStep'))]),
-    t.memberExpression(t.identifier(testCaseVar), t.identifier('testSteps')),
-    t.blockStatement([
-      // await testStep.setup()
-      t.expressionStatement(
-        t.awaitExpression(
-          withLoc(
-            t.callExpression(
-              t.memberExpression(t.identifier('testStep'), t.identifier('setup')),
-              []
+function createSuite(gherkin: CompiledGherkin) {
+  const suiteName = gherkin.gherkinDocument.feature?.name ?? gherkin.gherkinDocument.uri ?? ''
+  return t.expressionStatement(
+    // suite(suiteName, async () => { ... })
+    t.callExpression(t.identifier('suite'), [
+      t.stringLiteral(suiteName),
+      t.arrowFunctionExpression(
+        [],
+        t.blockStatement([
+          // const plan = await prepare(gherkin)
+          t.variableDeclaration('const', [
+            t.variableDeclarator(
+              t.identifier('plan'),
+              t.awaitExpression(t.callExpression(t.identifier('prepare'), [t.valueToNode(gherkin)]))
             ),
-            sourceLocation
-          )
-        )
+          ]),
+          ...gherkin.pickles.flatMap((pickle, index) => createTestCase(pickle, index)),
+        ]),
+        true
       ),
-      // await ctx1.test(testStep.name, testStep.options, async (ctx2) => { ... })
-      t.expressionStatement(
-        t.awaitExpression(
-          withLoc(
-            t.callExpression(t.memberExpression(t.identifier('ctx1'), t.identifier('test')), [
-              t.memberExpression(t.identifier('testStep'), t.identifier('name')),
-              t.memberExpression(t.identifier('testStep'), t.identifier('options')),
-              t.arrowFunctionExpression(
-                [t.identifier('ctx2')],
-                t.blockStatement([
-                  t.expressionStatement(
-                    t.awaitExpression(
-                      withLoc(
-                        t.callExpression(
-                          t.memberExpression(t.identifier('testStep'), t.identifier('execute')),
-                          [t.identifier('ctx2')]
-                        ),
-                        sourceLocation
-                      )
-                    )
-                  ),
-                ]),
-                true // async
-              ),
-            ]),
-            sourceLocation
-          )
-        )
-      ),
-      // await testStep.teardown()
-      t.expressionStatement(
-        t.awaitExpression(
-          withLoc(
-            t.callExpression(
-              t.memberExpression(t.identifier('testStep'), t.identifier('teardown')),
-              []
-            ),
-            sourceLocation
-          )
-        )
-      ),
-    ]),
-    true // await
+    ])
   )
 }
 
-function createTestBodyStatements(testCaseVar: string, sourceLocation: t.SourceLocation) {
-  return [
-    // await testCaseN.setup(ctx1)
-    t.expressionStatement(
-      t.awaitExpression(
-        withLoc(
-          t.callExpression(t.memberExpression(t.identifier(testCaseVar), t.identifier('setup')), [
-            t.identifier('ctx1'),
-          ]),
-          sourceLocation
-        )
-      )
-    ),
-    // for await (const testStep of testCaseN.testSteps) { ... }
-    createTestStepLoop(testCaseVar, sourceLocation),
-    // await testCaseN.teardown()
-    t.expressionStatement(
-      t.awaitExpression(
-        withLoc(
-          t.callExpression(
-            t.memberExpression(t.identifier(testCaseVar), t.identifier('teardown')),
-            []
-          ),
-          sourceLocation
-        )
-      )
-    ),
-  ]
-}
-
-function createTestCaseStatements(pickleId: string, pickleUri: string, index: number) {
+function createTestCase(pickle: Pickle, index: number) {
   const testCaseVar = `testCase${index}`
-  const sourceLocation = createSourceLocation(pickleUri)
+  const location = createPickleLocation(pickle)
 
   return [
     // const testCaseN = plan.select(pickleId)
@@ -139,9 +76,9 @@ function createTestCaseStatements(pickleId: string, pickleUri: string, index: nu
         t.identifier(testCaseVar),
         withLoc(
           t.callExpression(t.memberExpression(t.identifier('plan'), t.identifier('select')), [
-            t.stringLiteral(pickleId),
+            t.stringLiteral(pickle.id),
           ]),
-          sourceLocation
+          location
         )
       ),
     ]),
@@ -153,65 +90,120 @@ function createTestCaseStatements(pickleId: string, pickleUri: string, index: nu
             t.memberExpression(t.identifier(testCaseVar), t.identifier('name')),
             t.arrowFunctionExpression(
               [t.identifier('ctx1')],
-              t.blockStatement(createTestBodyStatements(testCaseVar, sourceLocation)),
-              true // async
+              t.blockStatement([
+                // await testCaseN.setup(ctx1)
+                t.expressionStatement(
+                  t.awaitExpression(
+                    withLoc(
+                      t.callExpression(
+                        t.memberExpression(t.identifier(testCaseVar), t.identifier('setup')),
+                        [t.identifier('ctx1')]
+                      ),
+                      location
+                    )
+                  )
+                ),
+                // for await (const testStep of testCaseN.testSteps) { ... }
+                t.forOfStatement(
+                  t.variableDeclaration('const', [t.variableDeclarator(t.identifier('testStep'))]),
+                  t.memberExpression(t.identifier(testCaseVar), t.identifier('testSteps')),
+                  t.blockStatement([
+                    // await testStep.setup()
+                    t.expressionStatement(
+                      t.awaitExpression(
+                        withLoc(
+                          t.callExpression(
+                            t.memberExpression(t.identifier('testStep'), t.identifier('setup')),
+                            []
+                          ),
+                          location
+                        )
+                      )
+                    ),
+                    // await ctx1.test(testStep.name, testStep.options, async (ctx2) => { ... })
+                    t.expressionStatement(
+                      t.awaitExpression(
+                        withLoc(
+                          t.callExpression(
+                            t.memberExpression(t.identifier('ctx1'), t.identifier('test')),
+                            [
+                              t.memberExpression(t.identifier('testStep'), t.identifier('name')),
+                              t.memberExpression(t.identifier('testStep'), t.identifier('options')),
+                              t.arrowFunctionExpression(
+                                [t.identifier('ctx2')],
+                                t.blockStatement([
+                                  // await testStep.execute(ctx2)
+                                  t.expressionStatement(
+                                    t.awaitExpression(
+                                      withLoc(
+                                        t.callExpression(
+                                          t.memberExpression(
+                                            t.identifier('testStep'),
+                                            t.identifier('execute')
+                                          ),
+                                          [t.identifier('ctx2')]
+                                        ),
+                                        location
+                                      )
+                                    )
+                                  ),
+                                ]),
+                                true
+                              ),
+                            ]
+                          ),
+                          location
+                        )
+                      )
+                    ),
+                    // await testStep.teardown()
+                    t.expressionStatement(
+                      t.awaitExpression(
+                        withLoc(
+                          t.callExpression(
+                            t.memberExpression(t.identifier('testStep'), t.identifier('teardown')),
+                            []
+                          ),
+                          location
+                        )
+                      )
+                    ),
+                  ]),
+                  true
+                ),
+                // await testCaseN.teardown()
+                t.expressionStatement(
+                  t.awaitExpression(
+                    withLoc(
+                      t.callExpression(
+                        t.memberExpression(t.identifier(testCaseVar), t.identifier('teardown')),
+                        []
+                      ),
+                      location
+                    )
+                  )
+                ),
+              ]),
+              true
             ),
           ]),
-          sourceLocation
+          location
         )
       )
     ),
   ]
 }
 
-function createSuiteBody(gherkin: CompiledGherkin) {
-  return [
-    // const plan = await prepare(gherkin)
-    t.variableDeclaration('const', [
-      t.variableDeclarator(
-        t.identifier('plan'),
-        t.awaitExpression(t.callExpression(t.identifier('prepare'), [t.valueToNode(gherkin)]))
-      ),
-    ]),
-    // Test cases for each pickle
-    ...gherkin.pickles.flatMap((pickle, index) =>
-      createTestCaseStatements(pickle.id, pickle.uri, index)
-    ),
-  ]
+function createPickleLocation(pickle: Pickle): t.SourceLocation {
+  return {
+    start: { line: 1, column: 0, index: 0 },
+    end: { line: 1, column: 0, index: 0 },
+    filename: path.basename(pickle.uri),
+    identifierName: undefined,
+  }
 }
 
-export function generateCode(gherkin: CompiledGherkin): string {
-  const featureName = gherkin.gherkinDocument.feature?.name ?? gherkin.gherkinDocument.uri ?? ''
-
-  const program = t.program([
-    ...createImports(),
-    // suite(featureName, async () => { ... })
-    t.expressionStatement(
-      t.callExpression(t.identifier('suite'), [
-        t.stringLiteral(featureName),
-        t.arrowFunctionExpression(
-          [],
-          t.blockStatement(createSuiteBody(gherkin)),
-          true // async
-        ),
-      ])
-    ),
-  ])
-
-  // Generate code from AST with source maps
-  const output = generate(
-    program,
-    {
-      retainLines: false,
-      compact: false,
-      sourceMaps: true,
-      sourceFileName: gherkin.source.uri,
-    },
-    gherkin.source.data
-  )
-
-  // Include inline source map for debugging
-  const sourceMapComment = `//# sourceMappingURL=data:application/json;base64,${Buffer.from(JSON.stringify(output.map)).toString('base64')}`
-
-  return `${output.code}\n${sourceMapComment}`
+function withLoc<T extends t.Node>(node: T, loc: t.SourceLocation): T {
+  node.loc = loc
+  return node
 }
