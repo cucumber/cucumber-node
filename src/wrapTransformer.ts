bdee9d9bb1bdbe9dbefc11377c8a39c8117fa3ae
David Goss <david@davidgoss.co>
2025-11-25 19:18:58 +0000

    commentary

--- src/wrapTransformer.ts
+++ src/wrapTransformer.ts
@@ -2,6 +2,13 @@ import { NewParameterType } from '@cucumber/core'
 
 import { TestCaseContext, TransformerFunction } from './types.js'
 
+/*
+ * Turn the user-supplied transformer function into one we can provide to
+ * the ParameterTypeRegistry - which only supports a custom `this` for
+ * modifying behaviour - while maintaining our signature of prepending
+ * the context as the first arg, and setting the world as `this` for
+ * continuity when coming from cucumber-js
+ */
 export function wrapTransformer(
   transformer?: TransformerFunction
 ): NewParameterType['transformer'] {
