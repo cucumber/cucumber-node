1e0bb99145949a203f1defdeb65ee5ef09192749
David Goss <david@davidgoss.co>
2026-01-06 16:24:23 +0000

    document our loader verify script

--- test/loader/verify.ts
+++ test/loader/verify.ts
@@ -1,7 +1,14 @@
+/**
+ * Run this file to manually verify the output of our Gherkin module loader in your
+ * terminal. Includes syntax highlighting.
+ */
+
 import { load } from '../../src/loader/index.js'
 import { pathToFileURL } from 'node:url'
 import path from 'node:path'
 import { LoadFnOutput, LoadHookContext } from 'node:module'
+// @ts-expect-error incomplete types
+import { highlight } from '@babel/code-frame'
 
 const transpiled = await load(
   pathToFileURL(path.join(import.meta.dirname, './sample.feature')).toString(),
@@ -9,4 +16,4 @@ const transpiled = await load(
   () => ({}) as LoadFnOutput
 )
 
-process.stdout.write(transpiled.source + '\n')
+process.stdout.write('\n' + highlight(transpiled.source) + '\n')
