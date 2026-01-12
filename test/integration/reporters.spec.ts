a0646823073b3859828948913a5477b289ad5f8f
David Goss <david@davidgoss.co>
2026-01-12 17:21:56 +0000

    remove defunct test

--- test/integration/reporters.spec.ts
+++ test/integration/reporters.spec.ts
@@ -31,26 +31,6 @@ describe('Reporters', () => {
       expect(sanitised).to.include(`test at ${path.join('features', 'foo.feature')}:2:3`)
     })
 
-    it('does not emit messages as diagnostics if no cucumber reporters', async () => {
-      const harness = await makeTestHarness()
-      await harness.writeFile(
-        'features/first.feature',
-        `Feature:
-  Scenario:
-    Given a step
-    `
-      )
-      await harness.writeFile(
-        'features/steps.js',
-        `import { Given } from '@cucumber/node'
-  Given('a step', () => {})
-    `
-      )
-      const [output] = await harness.run('spec')
-      const sanitised = stripVTControlCharacters(output.trim())
-      expect(sanitised).not.to.include('@cucumber/messages:')
-    })
-
     it('provides a useful error for an ambiguous step', async () => {
       const harness = await makeTestHarness()
       await harness.writeFile(
