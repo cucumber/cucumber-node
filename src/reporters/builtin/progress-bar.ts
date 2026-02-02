16760fa8effc136117464252a16098cb9ece31b2
David Goss <david@davidgoss.co>
2026-02-02 19:34:40 +0000

    always act on the stream directly

--- src/reporters/builtin/progress-bar.ts
+++ src/reporters/builtin/progress-bar.ts
@@ -6,15 +6,13 @@ import {
 } from '@cucumber/pretty-formatter'
 
 import { enrichMessages } from '../enrichMessages.js'
-import { proxyStream } from '../proxyStream.js'
 
 const options: ProgressBarOptions = {
 }
 
 export default async function* (events: AsyncIterable<TestEvent>): AsyncGenerator<string> {
   const buffer: Array<string> = []
-  const stream = proxyStream(process.stdout, (content: string) => buffer.push(content))
-  const printer = new ProgressBarPrinter({ stream, options })
+  const printer = new ProgressBarPrinter({ stream: process.stdout, options })
   const envelopes = enrichMessages(events)
   for await (const envelope of envelopes) {
     printer.update(envelope)
